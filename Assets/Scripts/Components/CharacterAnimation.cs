662aeb5c40043e9e6220a25b86cf5eafd6c65f5a
Valentin Pavlov <v.pavlov@xlab-game.com>
2023-03-24 14:42:19 +0300

    GameOver

--- Assets/Scripts/Components/CharacterAnimation.cs
+++ Assets/Scripts/Components/CharacterAnimation.cs
@@ -9,9 +9,11 @@ namespace TopDownShooter
 		private Animator m_animator;
 		private AttackManager m_attackManager;
 		private MovingComponent m_moving;
+		private HealthComponent m_health;
 
 		static int SpeedMoveId = Animator.StringToHash("SpeedMove");
 		static int AttackId = Animator.StringToHash("Attack");
+		static int DieId = Animator.StringToHash("Die");
 
 		private Vector3 m_lastPosition;
 
@@ -21,16 +23,24 @@ namespace TopDownShooter
 			m_animator = GetComponent<Animator>();
 			m_attackManager = GetComponentInParent<AttackManager>();
 			m_moving = GetComponentInParent<MovingComponent>();
+			m_health = GetComponentInParent<HealthComponent>();
 		}
 
 		private void OnEnable()
 		{
 			m_attackManager.onAttack.AddListener(OnAttackHandler);
+			m_health.onDead += OnDead;
 		}
 
 		private void OnDisable()
 		{
+			m_health.onDead -= OnDead;
 			m_attackManager.onAttack.RemoveListener(OnAttackHandler);
+		}
+
+		private void OnDead()
+		{
+			m_animator.SetTrigger(DieId);
 		}
 
 		private void OnAttackHandler()
--- Assets/Scripts/States/GameOverState.cs
+++ Assets/Scripts/States/GameOverState.cs
@@ -1,9 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 namespace TopDownShooter.States
 {
 	public class GameOverState : GameStateBehavior
 	{
-		
+		protected override void OnEnter()
+		{
+			StartCoroutine(ExitToMainMenu());
+		}
+
+		private IEnumerator ExitToMainMenu()
+		{
+			yield return new WaitForSecondsRealtime(3f);
+			
+			GameController.LoadScene("MainMenu");
+		}
 	}
 }
