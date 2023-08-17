8226988df614553c0f402c555ea3e8add9da482e
Valentin Pavlov <v.pavlov@xlab-game.com>
2023-08-17 18:41:43 +0300

    add car

---
+++ Assets/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public InputActionReference TurnInput;
+        public InputActionReference AccelerateButton;
+        public InputActionReference BrakeButton;
+
+        private void Awake()
+        {
+            TurnInput.action.Enable();
+            AccelerateButton.action.Enable();
+            BrakeButton.action.Enable();
+        }
+
+        public override InputData GenerateInput() {
+            return new InputData
+            {
+                Accelerate = AccelerateButton.action.IsPressed(),
+                Brake = BrakeButton.action.IsPressed(),
+                TurnInput = TurnInput.action.ReadValue<float>()
+            };
+        }
+    }
+}
